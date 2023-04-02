01b462e9676d210a8e8a00432ae69ce700829109
GalaDriel <g_ala@naver.com>
2023-04-02 15:18:46 +0900

    Update: Import data and combine with templates

--- 01.Pyburger/config/views.py
+++ 01.Pyburger/config/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from burgers.models import Burger
 
 def main(request):
     return render(request , 'main.html')
 
 def burger(request):
-    return render(request , 'burger.html')
+    burger = Burger.objects.all()
+    context = {
+        "burgers" : burger
+    }
+    return render(request , 'burger.html' , context)
