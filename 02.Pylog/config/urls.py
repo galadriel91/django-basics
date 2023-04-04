3051b604429c951c5d6f6de893d158058a15cc1d
GalaDriel <g_ala@naver.com>
2023-04-04 14:07:22 +0900

    Update: Define thumbnail model in Post and binding with template

--- 02.Pylog/config/urls.py
+++ 02.Pylog/config/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from config.views import main, add, detail, post
@@ -25,3 +27,9 @@ urlpatterns = [
     path('detail/' , detail),
     path('add/' , add)
 ]
+
+urlpatterns += static(
+    prefix=settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
+
