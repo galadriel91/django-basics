8281467079e4b4df473562ff9aeb11bedbcf2f89
GalaDriel <g_ala@naver.com>
2023-04-10 11:30:03 +0900

    Update: Setup Burger app & Migrate model

---
+++ 01.Pyburger/burger/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from burger.models import Burger
+
+
+# Register your models here.
+@admin.register(Burger)
+class BurgerAdmin(admin.ModelAdmin):
+    list_display = ['title']
