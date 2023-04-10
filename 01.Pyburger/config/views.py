09f17bddb1790e4e23bd9a8e46537ca543bbf004
GalaDriel <g_ala@naver.com>
2023-04-10 14:43:44 +0900

    Update: Define search template

--- 01.Pyburger/config/views.py
+++ 01.Pyburger/config/views.py
@@ -14,4 +14,12 @@ def lists(request):
     return render(request , 'list.html' , context)
 
 def search(requeset):
-    return render(requeset , 'search.html')
+    keyword = requeset.GET.get('keyword')
+    if keyword is not None :
+        item = Burger.objects.filter(title__contains = keyword)
+    else:
+        item = Burger.objects.none()
+    context = {
+        'item': item
+    }
+    return render(requeset, 'search.html', context)
