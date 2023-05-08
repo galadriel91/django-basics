ae5e186a2c87c48888aebc985df9eb23fd07ef67
GalaDriel <g_ala@naver.com>
2023-05-08 23:01:45 +0900

    Update: Define staticfiles setting

--- 01.PyBurger/burger/views.py
+++ 01.PyBurger/burger/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from .models import Burger
 # Create your views here.
 
 def burger_list(request):
-    return render(request, 'burger_list.html')
+    burgers = Burger.objects.all()
+    context = {
+        'burgers' : burgers
+    }
+    return render(request, 'burger_list.html' , context)
