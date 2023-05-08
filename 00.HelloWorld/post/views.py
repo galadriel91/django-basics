a088b1d0169cc15034f5bfcb913c4ea392b064b5
GalaDriel <g_ala@naver.com>
2023-05-08 09:48:10 +0900

    Update: Define post_detail.html urls

--- 00.HelloWorld/post/views.py
+++ 00.HelloWorld/post/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'post/post_list.html')
+    posts = Post.objects.all()
+    context = {
+        "posts" : posts
+    }
+    return render(request, 'post/post_list.html', context)
+    
+def post_detail(request , pk):
+    post = get_object_or_404(Post, pk=pk)
+    context = {
+        "post" : post
+    }
+    return render(request, 'post/post_detail.html', context)    
