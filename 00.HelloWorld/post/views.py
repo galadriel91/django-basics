f89b966a62a932163943bd92cab56c3517332df9
GalaDriel <g_ala@naver.com>
2023-05-08 10:05:10 +0900

    Update: Define post_add template urls

--- 00.HelloWorld/post/views.py
+++ 00.HelloWorld/post/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render , get_object_or_404
+from django.shortcuts import render , get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -16,3 +17,17 @@ def post_detail(request , pk):
         "post" : post
     }
     return render(request, 'post/post_detail.html', context)    
+
+def post_add(request):
+    if request.method == 'POST':
+        forms = PostForm(request.POST)
+        if forms.is_valid:
+            post = forms.save(commit=False)
+            post.save()
+            return redirect('post_detail' , pk=post.pk)
+    else:
+        forms = PostForm()    
+    context = {
+        'forms' : forms
+    }
+    return render(request, 'post/post_add.html', context)    
