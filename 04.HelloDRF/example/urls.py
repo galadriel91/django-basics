02b66bbb7ae292e2047db3f0c7cfd911bcc83dc7
GalaDriel <g_ala@naver.com>
2023-05-08 21:07:50 +0900

    Update: Define views with fbv

--- 04.HelloDRF/example/urls.py
+++ 04.HelloDRF/example/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from .views import HelloApi
+from .views import HelloApi, BooksApi, BookApi
 
 urlpatterns = [
     path('hello/', HelloApi),
+    path('fbv/books/', BooksApi),
+    path('fbv/books/<int:bid>/', BookApi),
 ]
--- 04.HelloDRF/example/views.py
+++ 04.HelloDRF/example/views.py
@@ -2,9 +2,31 @@ from rest_framework import viewsets, permissions, generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
+from .models import Book
+from .serializers import BookSerializers
 
 # Create your views here.
 
 @api_view(['GET'])
 def HelloApi(request):
-    return Response('Hello World')
+    return Response('Hello Djanogo!')
+
+@api_view(['GET', 'POST'])
+def BooksApi(request):
+    if request.method == 'GET':
+        books = Book.objects.all()
+        serializer = BookSerializers(books, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = BookSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def BookApi(request, bid):
+    book = get_object_or_404(Book, bid=bid)
+    serializer = BookSerializers(book)
+    return Response(serializer.data, status=status.HTTP_200_OK)
