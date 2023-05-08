a2825d2ef7a5f5857f486acb39acd719277ccffe
GalaDriel <g_ala@naver.com>
2023-05-08 15:09:04 +0900

    Update: Define todo_list.html url & view

--- 03.PyTodo/todo/views.py
+++ 03.PyTodo/todo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Todo
 
 # Create your views here.
+
+def todo_list(request):
+    todos = Todo.objects.all()
+    context={
+        'todos':todos
+    }
+    return render(request, 'todo/todo_list.html', context)
