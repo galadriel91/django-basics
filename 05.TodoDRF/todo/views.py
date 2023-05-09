4094aba2ff782b28a7d29b079843f8eca584dbb6
GalaDriel <g_ala@naver.com>
2023-05-09 12:50:50 +0900

    Update: Define TodosAPIView

--- 05.TodoDRF/todo/views.py
+++ 05.TodoDRF/todo/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import TodosSerializers
+from .models import Todo
 
 # Create your views here.
+
+class TodosAPIView(APIView):
+    def get(self, request):
+        todos = Todo.objects.filter(done=False)
+        serializer = TodosSerializers(todos, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
