4e51c167ef3b54183636c4122c3ec2c445eccf61
GalaDriel <g_ala@naver.com>
2023-05-10 08:07:24 +0900

    Update: Define Todo model & register admin

--- 05.TodoDRF/todo/models.py
+++ 05.TodoDRF/todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    important = models.BooleanField(default=False)
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
