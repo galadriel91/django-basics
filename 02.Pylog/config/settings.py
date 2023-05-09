62267631fe9e076d9bcf93285f5f467955d60268
GalaDriel <g_ala@naver.com>
2023-05-09 16:00:18 +0900

    Update: Define urls setting & templates

--- 02.Pylog/config/settings.py
+++ 02.Pylog/config/settings.py
@@ -22,7 +22,7 @@ env = environ.Env(
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = BASE_DIR / 'templates'
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- 02.Pylog/post/views.py
+++ 02.Pylog/post/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post, Comment
 
 # Create your views here.
+
+def PostList(request):
+    return render(request, 'post_list.html')
