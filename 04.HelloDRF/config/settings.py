6a64cebb100c2ba944860ce9eb83e6e6608c6ab3
GalaDriel <g_ala@naver.com>
2023-05-09 19:02:42 +0900

    <Django HelloDRF Reinitial Commit>

--- 04.HelloDRF/config/settings.py
+++ 04.HelloDRF/config/settings.py
@@ -20,12 +20,10 @@ env = environ.Env(
     DEBUG=(bool, False)
 )
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -47,8 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'example'
 ]
 
 MIDDLEWARE = [
--- 04.HelloDRF/example/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from .views import BooksApi, BookApi
-
-urlpatterns = [
-    path('fbv/books/', BooksApi.as_view()),
-    path('fbv/books/<int:bid>/', BookApi.as_view()),
-]
