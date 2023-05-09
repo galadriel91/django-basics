18ef217b33cf2e67dab3fc75c8ea60eeea07fc58
GalaDriel <g_ala@naver.com>
2023-05-09 15:53:43 +0900

    Update: Define Post, Comment models & admin

--- 02.Pylog/post/models.py
+++ 02.Pylog/post/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return f"{self.post.title}의 댓글 {self.id}입니다"    
