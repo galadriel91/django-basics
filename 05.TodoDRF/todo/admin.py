189f6c71fe394fd42645ccfa261683f516637a1a
GalaDriel <g_ala@naver.com>
2023-05-10 08:00:49 +0900

    <Django TodoDRF Reinitial Commit>

--- 05.TodoDRF/todo/admin.py
+++ 05.TodoDRF/todo/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import Todo
 
 # Register your models here.
-@admin.register(Todo)
-class TodoAdmin(admin.ModelAdmin):
-    pass
--- 05.TodoDRF/todo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import TodosAPIView, TodoAPIView, TodoDoneAPIView, CheckDoneAPIView
-
-urlpatterns = [
-    path('' , TodosAPIView.as_view()),
-    path('<int:pk>/' , TodoAPIView.as_view()),
-    path('done/', TodoDoneAPIView.as_view()),
-    path('done/<int:pk>', CheckDoneAPIView.as_view())
-]
