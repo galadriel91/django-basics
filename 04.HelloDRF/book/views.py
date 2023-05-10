37d33a7c8631df197b1e6fbbd94928fbed0cb150
GalaDriel <g_ala@naver.com>
2023-05-10 11:10:29 +0900

    Update: Apply generic in views

--- 04.HelloDRF/book/views.py
+++ 04.HelloDRF/book/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status, viewsets, generics, mixins
+from rest_framework import status, viewsets, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
@@ -8,23 +8,11 @@ from .serializers import BookSerializers
 # Create your views here.
 
 
-class BooksAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class BooksAPIView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializers
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
         
-class BookAPIView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class BookAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializers
     lookup_field = 'bid'
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
