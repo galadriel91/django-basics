1c20ce9dfec25984eaafe5f52e7d10364f4dd43d
GalaDriel <g_ala@naver.com>
2023-05-10 17:45:23 +0900

    Update: Define Login & Logout

--- 06.Pystagram/users/views.py
+++ 06.Pystagram/users/views.py
@@ -1,9 +1,36 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from .forms import LoginForm
 
 # Create your views here.
 
 def login_view(request):
-    user = request.user
-    if user.is_authenticated:
+    if request.user.is_authenticated:
         return redirect('Feeds')
-    return render(request, 'users/login.html')
+    
+    if request.method == 'POST':
+        form = LoginForm(data = request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username = username, password = password)
+
+            if user:
+                login(request, user)
+                return redirect('Feeds')
+            else:
+                form.add_error(None, "로그인에 실패했습니다.")
+        context = {
+            'form' : form
+        }
+        return render(request, 'users/login.html', context)        
+    else:
+        form = LoginForm()
+        context = {
+            'form' : form
+        }
+        return render(request, 'users/login.html', context)
+
+def logout_view(request):
+    logout(request)
+    return redirect('Login')
