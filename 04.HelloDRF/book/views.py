b8cf0866aa1f9b7f656def677aa667bf185d9b24
GalaDriel <g_ala@naver.com>
2023-05-10 10:33:07 +0900

    Update: Define BookSerializers & BookApiView

--- 04.HelloDRF/book/views.py
+++ 04.HelloDRF/book/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import status, viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
+from .models import Book
+from .serializers import BookSerializers
 # Create your views here.
+
+class BooksAPIView(APIView):
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializers(books, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
