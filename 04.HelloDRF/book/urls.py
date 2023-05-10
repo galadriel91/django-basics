9fef187817f404a5f4de9795ce29360d50d17d23
GalaDriel <g_ala@naver.com>
2023-05-10 10:39:02 +0900

    Update: Define BookApiView

--- 04.HelloDRF/book/urls.py
+++ 04.HelloDRF/book/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BooksAPIView
+from .views import BooksAPIView, BookAPIView
 
 urlpatterns = [
     path('', BooksAPIView.as_view()),
+    path('<int:pk>', BookAPIView.as_view()),
 ]
--- 04.HelloDRF/book/views.py
+++ 04.HelloDRF/book/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
 
 from .models import Book
 from .serializers import BookSerializers
@@ -10,4 +11,10 @@ class BooksAPIView(APIView):
     def get(self, request):
         books = Book.objects.all()
         serializer = BookSerializers(books, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
+class BookAPIView(APIView):
+    def get(self, request, pk):
+        books = get_object_or_404(Book, bid = pk)
+        serializer = BookSerializers(books)
         return Response(serializer.data, status=status.HTTP_200_OK)
