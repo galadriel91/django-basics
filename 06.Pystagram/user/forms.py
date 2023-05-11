a3b1c3d0ca36e54fcb8a42663e9d257d05a22c29
GalaDriel <g_ala@naver.com>
2023-05-11 10:11:11 +0900

    Update: Define signup_view

--- 06.Pystagram/user/forms.py
+++ 06.Pystagram/user/forms.py
@@ -3,3 +3,10 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(min_length=3 , widget=forms.TextInput(attrs={"placeholder" : "사용자명 3자리 이상"}))
     password = forms.CharField(min_length=4 , widget=forms.PasswordInput(attrs={"placeholder": "비밀번호를 입력해주세요"}))
+
+class SignupForm(forms.Form):
+    username = forms.CharField(min_length=3)
+    password = forms.CharField(min_length=4)
+    password1 = forms.CharField(min_length=4)
+    profile_image = forms.ImageField()
+    short_description = forms.CharField()
--- 06.Pystagram/user/views.py
+++ 06.Pystagram/user/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-from .forms import LoginForm
+from .forms import LoginForm, SignupForm
+from .models import User
 
 # Create your views here.
 
@@ -33,3 +34,38 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('Login')
+
+def signup_view(request):
+    if request.method == 'POST':
+        forms = SignupForm(data = request.POST, files= request.FILES)
+        if forms.is_valid():
+            username = forms.cleaned_data['username']
+            password = forms.cleaned_data['password']
+            password1 = forms.cleaned_data['password1']
+            profile_image = forms.cleaned_data['profile_image']
+            short_description = forms.cleaned_data['short_description']
+            
+            if password != password1:
+                forms.add_error('password1' , '비밀번호가 다릅니다')
+
+            if User.objects.filter(username = username).exists():
+                forms.add_error('username' , '이미 존재하는 사용자입니다.')
+
+            if forms.errors:
+                context = {'forms': forms}
+                return render(request, 'user/signup_view.html' , context)
+            else:
+                user = User.objects.create_user(
+                    username = username,
+                    password = password1,
+                    profile_image = profile_image,
+                    short_description = short_description,
+                )
+                login(request, user)
+                return redirect('Feed')
+    else:
+        forms = SignupForm()
+        context= { 
+            'forms' : forms
+        }
+        return render(request, 'user/signup_view.html' , context)
