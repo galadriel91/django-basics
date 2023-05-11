53dee2cf9ba475a575649f5b9e0ca238ce067661
GalaDriel <g_ala@naver.com>
2023-05-11 21:17:46 +0900

    Update: Define LoginForm & LoginView

--- 06.Pystagram/config/urls.py
+++ 06.Pystagram/config/urls.py
@@ -21,6 +21,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('posts/', include('posts.urls')),
 ]
 
 urlpatterns += static(
--- 06.Pystagram/users/views.py
+++ 06.Pystagram/users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout , authenticate
+from django.shortcuts import render, redirect
+from .forms import LoginForm
 
 # Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        forms = LoginForm(data=request.POST)
+        if forms.is_valid():
+            username = forms.cleaned_data['username']
+            password = forms.cleaned_data['password']
+
+            user = authenticate(username = username, password = password)
+            
+            if user:
+                login(request, user)
+                return redirect('Feeds')
+            else:
+                forms.add_error('로그인에 실패했습니다.')
+        context={'forms':forms}
+        return render(request, 'users/login_view.html' , context)                
+    else:
+        forms = LoginForm()
+        context={'forms':forms}
+        return render(request, 'users/login_view.html' , context)
