05395d425108fd34b74a513a857ba50b223351f0
GalaDriel <g_ala@naver.com>
2023-05-11 20:02:37 +0900

    <Django Pylog Reinitial Commit>

--- 02.Pylog/config/settings.py
+++ 02.Pylog/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+
 import environ
 import os
 
@@ -19,15 +20,13 @@ env = environ.Env(
     DEBUG=(bool, False)
 )
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = BASE_DIR / 'templates'
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
@@ -46,7 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'post'
 ]
 
 MIDDLEWARE = [
@@ -126,13 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[BASE_DIR / 'static']
+STATICFILES_DIR = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- 02.Pylog/post/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import PostList, PostDetail, PostAdd
-
-urlpatterns = [
-    path('', PostList, name='PostList'),
-    path('<int:pk>/', PostDetail, name='PostDetail'),
-    path('add/', PostAdd, name='PostAdd'),
-]
