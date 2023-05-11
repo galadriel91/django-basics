cf5d969d5c32ac6648e79c41255a32ba8af1d566
GalaDriel <g_ala@naver.com>
2023-05-11 22:09:20 +0900

    Update: Define SignupView

--- 06.Pystagram/users/forms.py
+++ 06.Pystagram/users/forms.py
@@ -3,3 +3,10 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(min_length=3)
     password = forms.CharField(min_length=4)
+
+class SignupForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
+    password1 = forms.CharField()
+    profile_image = forms.ImageField()
+    short_description = forms.CharField()
