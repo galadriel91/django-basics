76b38f259b8e4257500a6279e9a9f5256e5bae35
GalaDriel <g_ala@naver.com>
2023-05-11 09:17:02 +0900

    Update: Apply index.css

--- 06.Pystagram/user/forms.py
+++ 06.Pystagram/user/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField(min_length=3)
-    password = forms.CharField(min_length=4)
+    username = forms.CharField(min_length=3 , widget=forms.TextInput(attrs={"placeholder" : "사용자명 3자리 이상"}))
+    password = forms.CharField(min_length=4 , widget=forms.PasswordInput(attrs={"placeholder": "비밀번호를 입력해주세요"}))
