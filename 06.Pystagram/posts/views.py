8381106774d5cd11542f646c5e7ea38c1da6626c
GalaDriel <g_ala@naver.com>
2023-05-12 14:20:47 +0900

    Update: Define Feeds_view basics layout

--- 06.Pystagram/posts/views.py
+++ 06.Pystagram/posts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
+from .models import Post
 
 # Create your views here.
 def feeds_view(request):
     if not request.user.is_authenticated:
         return redirect('Login')
-    return render(request, 'posts/feeds_view.html')
+    posts = Post.objects.all()
+    context={"posts" : posts}
+    return render(request, 'posts/feeds_view.html', context)
