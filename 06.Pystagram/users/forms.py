35e43031b868efa6e5651716339363ca3b154801
GalaDriel <g_ala@naver.com>
2023-05-13 15:40:48 +0900

    Update: Define SingupView

--- 06.Pystagram/users/forms.py
+++ 06.Pystagram/users/forms.py
@@ -1,5 +1,12 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField(min_length=4)
-    password = forms.CharField(min_length=3)
+    username = forms.CharField(min_length=4 , widget=forms.TextInput(attrs={'placeholder':'사용자명(3글자 이상)'}))
+    password = forms.CharField(min_length=3 , widget=forms.PasswordInput(attrs={'placeholder':'비밀번호(5글자 이상)'}))
+
+class SignupForm(forms.Form):
+    username = forms.CharField(min_length=3)    
+    password = forms.CharField(min_length=5)    
+    password1 = forms.CharField(min_length=5)    
+    profile_image = forms.ImageField()    
+    short_description = forms.CharField()    
