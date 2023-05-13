7eb3c2ebf0202b5f7e80fc65c33599147ff5c9b8
GalaDriel <g_ala@naver.com>
2023-05-13 18:05:21 +0900

    Update: Define CommentDelete & CommentAdd

--- 06.Pystagram/posts/urls.py
+++ 06.Pystagram/posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import feeds_view
+from .views import feeds_view, comment_add, comment_delete
 
 urlpatterns = [
     path('feed/', feeds_view, name='Feeds'),
+    path('comment_add/', comment_add, name='CommentAdd'),
+    path('comment_delete/<int:comment_id>', comment_delete, name='CommentDelete'),
 ]
--- 06.Pystagram/posts/views.py
+++ 06.Pystagram/posts/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render , redirect
+from django.http import HttpResponseRedirect, HttpResponseForbidden
+from django.views.decorators.http import require_POST
+from .models import Post, Comment
+from .forms import CommentForm
 
 # Create your views here.
 def feeds_view(request):
+    comment_form = CommentForm()
     posts = Post.objects.all()
-    context={'posts':posts}
+    context={'posts':posts, 'comment_form': comment_form}
     return render(request, 'posts/feeds_view.html' , context)
+
+@require_POST
+def comment_add(request):
+    forms = CommentForm(data = request.POST)
+    if forms.is_valid():
+        comment = forms.save(commit=False)
+        comment.user = request.user
+        comment.save()
+        return redirect('Feeds')
+
+@require_POST
+def comment_delete(request, comment_id):
+    comment = Comment.objects.get(id = comment_id)
+    if request.user == comment.user:
+        comment.delete()
+        return redirect('Feeds')
+    else:
+        return HttpResponseForbidden('이 게시글을 삭제 할 수 없습니다.')
