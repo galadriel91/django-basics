2fcca9a6a693a5018c6a1048151a58dbe791f2d4
GalaDriel <g_ala@naver.com>
2023-05-13 16:07:00 +0900

    Update: Define SignupForm

--- 06.Pystagram/users/forms.py
+++ 06.Pystagram/users/forms.py
@@ -1,12 +1,39 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from .models import User
 
 class LoginForm(forms.Form):
     username = forms.CharField(min_length=4 , widget=forms.TextInput(attrs={'placeholder':'사용자명(3글자 이상)'}))
     password = forms.CharField(min_length=3 , widget=forms.PasswordInput(attrs={'placeholder':'비밀번호(5글자 이상)'}))
 
 class SignupForm(forms.Form):
-    username = forms.CharField(min_length=3)    
-    password = forms.CharField(min_length=5)    
-    password1 = forms.CharField(min_length=5)    
+    username = forms.CharField(min_length=3, widget=forms.TextInput(attrs={'placeholder':'사용자명(3글자 이상)'}))    
+    password = forms.CharField(min_length=5 , widget=forms.PasswordInput(attrs={'placeholder':'비밀번호(5글자 이상)'}))    
+    password1 = forms.CharField(min_length=5, widget=forms.PasswordInput(attrs={'placeholder':'비밀번호(5글자 이상)'}))    
     profile_image = forms.ImageField()    
     short_description = forms.CharField()    
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username = username).exists():
+            raise ValidationError('이미 존재하는 사용자 입니다')
+        return username
+
+    def clean(self):
+        password = self.cleaned_data['password']
+        password1 = self.cleaned_data['password1']
+        if password != password1:
+            self.add_error('password1' , '비밀번호가 맞지않습니다.')
+
+    def save(self):
+        username = self.cleaned_data['username']        
+        password = self.cleaned_data['password1']        
+        profile_image = self.cleaned_data['profile_image']        
+        short_description = self.cleaned_data['short_description']
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            profile_image=profile_image,
+            short_description=short_description
+        )        
+        return user
--- 06.Pystagram/users/views.py
+++ 06.Pystagram/users/views.py
@@ -25,35 +25,13 @@ def login_view(request):
         return render(request, 'users/login_view.html' , context)
     
 def signup_view(request):
-
     if request.POST:
         forms = SignupForm(data = request.POST, files=request.FILES)
         if forms.is_valid():
-            username = forms.cleaned_data['username']
-            password = forms.cleaned_data['password']
-            password1 = forms.cleaned_data['password1']
-            profile_image = forms.cleaned_data['profile_image']
-            short_description = forms.cleaned_data['short_description']
-
-            if password != password1:
-                forms.add_error('password1' , '비밀번호가 맞지않습니다.')
-
-            if User.objects.filter(username=username).exists():
-                forms.add_error('username' , '이미 존재하는 사용자입니다.')
-            
-            if forms.errors:
-                context={'forms':forms}
-                return render(request, 'users/signup_view.html' , context)
-            else:
-                user = User.objects.create_user(
-                    username = username,
-                    password = password1,
-                    profile_image = profile_image,
-                    short_description = short_description
-                )    
-                login(request, user)
-                return redirect('Feeds')
+            user = forms.save()
+            login(request, user)
+            return redirect('Feeds')
     else:
         forms = SignupForm()
-        context={'forms':forms}
-        return render(request, 'users/signup_view.html' , context)
+    context={'forms':forms}
+    return render(request, 'users/signup_view.html' , context)
