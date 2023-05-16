9a117d5c9b2d20cd9f7bc4914d4ad236f0f7db12
GalaDriel <g_ala@naver.com>
2023-05-16 11:44:03 +0900

    Update: Apply generic in BooksView & BookView

--- 04.HelloDRF/example/urls.py
+++ 04.HelloDRF/example/urls.py
@@ -3,5 +3,5 @@ from .views import BooksView, BookView
 
 urlpatterns = [
     path('', BooksView.as_view()),
-    path('<int:pk>', BookView.as_view()),
+    path('<int:bid>/', BookView.as_view()),
 ]
--- 04.HelloDRF/example/views.py
+++ 04.HelloDRF/example/views.py
@@ -1,4 +1,4 @@
-from rest_framework import views, status
+from rest_framework import views, status, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
@@ -6,20 +6,11 @@ from .models import Book
 from .serializers import BookSerializers
 # Create your views here.
 
-class BooksView(APIView):
-    def get(self, request):
-        books = Book.objects.all()
-        serializer = BookSerializers(books, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    def post(self, request):
-        serializer = BookSerializers(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-class BookView(APIView):
-    def get(self, request , pk):
-        book = get_object_or_404(Book, pk=pk)
-        serializer = BookSerializers(book)
-        return Response(serializer.data, status=status.HTTP_200_OK)    
+class BooksView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializers
+
+class BookView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializers
+    lookup_field = 'bid'        
