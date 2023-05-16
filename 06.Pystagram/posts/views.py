e7909620d065a6dbdccd0a5742af739526a41269
GalaDriel <g_ala@naver.com>
2023-05-16 20:22:27 +0900

    Update: Define feeds_view.html article

--- 06.Pystagram/posts/views.py
+++ 06.Pystagram/posts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Post, PostImage, Comment
 
 # Create your views here.
 def feedView(request):
-    return render(request, 'posts/feeds_view.html')
+    posts = Post.objects.all()
+    context={'posts':posts}
+    return render(request, 'posts/feeds_view.html' , context)
