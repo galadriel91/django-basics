9f07efe30ad25f23dd08fe73232eed15eb7fbf10
GalaDriel <g_ala@naver.com>
2023-05-16 13:57:06 +0900

    Update: Define TodoSerializers & TodosApiview

--- 05.TodoDRF/todo/models.py
+++ 05.TodoDRF/todo/models.py
@@ -5,8 +5,8 @@ class Todo(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    important = models.BooleanField(default=True)
-    done = models.BooleanField(default=True)
+    important = models.BooleanField(default=False)
+    done = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
--- 05.TodoDRF/todo/views.py
+++ 05.TodoDRF/todo/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import views, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import TodoSerializers
+from .models import Todo
 
 # Create your views here.
+class TodosAPIView(APIView):
+    def get(self, request):
+        todos = Todo.objects.all()
+        serializer = TodoSerializers(todos, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
