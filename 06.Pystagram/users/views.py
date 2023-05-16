655d12089763d1f2eb48c3e40ef5299cc2820c89
GalaDriel <g_ala@naver.com>
2023-05-16 17:28:34 +0900

    Update: Refactoring signupform

--- 06.Pystagram/users/views.py
+++ 06.Pystagram/users/views.py
@@ -28,35 +28,13 @@ def signupView(request):
     if request.method == 'POST':
         forms = SignupForm(data = request.POST, files=request.FILES)
         if forms.is_valid():
-            username = forms.cleaned_data['username']
-            password = forms.cleaned_data['password']
-            password1 = forms.cleaned_data['password1']
-            profile_image = forms.cleaned_data['profile_image']
-            description = forms.cleaned_data['description']
-
-        if password != password1:
-            forms.add_error('password1' , '비밀번호가 맞지 않습니다.')
-
-        if User.objects.filter(username = username).exists():
-            forms.add_error('username', '이미 존재하는 사용자 입니다.')
-
-        if forms.errors:
-            context={'forms':forms}
-            return render(request, 'users/signup_view.html', context)    
-        else:
-            user = User.objects.create_user(
-                username = username,
-                password = password1,
-                profile_image = profile_image,
-                description = description
-            )
+            user = forms.save()
             login(request, user)
             return redirect('Feeds')
-
     else:
         forms = SignupForm()
-        context={'forms':forms}
-        return render(request, 'users/signup_view.html', context)
+    context={'forms':forms}
+    return render(request, 'users/signup_view.html', context)
 
 def logoutView(requet):
     logout(requet)
