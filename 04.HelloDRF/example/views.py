dc71da449861c921a4bc07f27615b089f94d33e2
GalaDriel <g_ala@naver.com>
2023-05-16 11:30:04 +0900

    Update: BooksView - post

--- 04.HelloDRF/example/views.py
+++ 04.HelloDRF/example/views.py
@@ -11,6 +11,12 @@ class BooksView(APIView):
         books = Book.objects.all()
         serializer = BookSerializers(books, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    def post(self, request):
+        serializer = BookSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class BookView(APIView):
     def get(self, request , pk):
