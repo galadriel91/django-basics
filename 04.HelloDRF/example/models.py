61fbb5f31c00a2fe84b390b6570551c26b35fe6d
GalaDriel <g_ala@naver.com>
2023-05-16 10:43:25 +0900

    Update: Define Book model & Book admin

--- 04.HelloDRF/example/models.py
+++ 04.HelloDRF/example/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    bid = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    price = models.IntegerField()
+    pages = models.IntegerField()
+    published_date = models.DateField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
