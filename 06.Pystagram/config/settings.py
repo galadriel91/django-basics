052780ddbcd3faffe16b14aaa031dc1ca2b43774
GalaDriel <g_ala@naver.com>
2023-05-16 16:06:06 +0900

    Update: Define User model & admin

--- 06.Pystagram/config/settings.py
+++ 06.Pystagram/config/settings.py
@@ -22,7 +22,7 @@ env = environ.Env(
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+AUTH_USER_MODEL = 'users.User'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users'
 ]
 
 MIDDLEWARE = [
