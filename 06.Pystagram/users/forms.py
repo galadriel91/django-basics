18c53d5173379f761427e0d95d6ba59ef75dcfa3
GalaDriel <g_ala@naver.com>
2023-06-13 23:27:37 +0900

    Update: Define Signup logic

--- 06.Pystagram/users/forms.py
+++ 06.Pystagram/users/forms.py
@@ -9,3 +9,10 @@ class LoginForm(forms.Form):
         min_length=4,
         widget=forms.TextInput(attrs={'placeholder': '비밀번호(4글자 이상)'}),
     )
+
+class SignupForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    profile_image = forms.ImageField()
+    short_desc = forms.CharField()
--- 06.Pystagram/users/views.py
+++ 06.Pystagram/users/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-from .forms import LoginForm
-
+from .forms import LoginForm, SignupForm
+from .models import User
 # Create your views here.
 
 def login_view(request):
@@ -17,7 +17,7 @@ def login_view(request):
                 login(request, user)
                 return redirect('feeds')
             else:
-                form.add_error('None' , '로그인에 실패했습니다.')
+                form.add_error(None , '로그인에 실패했습니다.')
         context= {
             'form' : form
         }
@@ -32,3 +32,40 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('login')
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignupForm(data = request.POST, files=request.FILES)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            password1 = form.cleaned_data['password1']
+            profile_image = form.cleaned_data['profile_image']
+            short_desc = form.cleaned_data['short_desc']
+
+            if password != password1:
+                form.add_error('password1' , '비밀번호가 맞지 않습니다.')
+
+            if User.objects.filter(username = username).exists():
+                form.add_error('username', '이미 존재하는 사용자입니다.')
+
+            if form.errors:
+                context={
+                    'form':form
+                }    
+                return render(request, 'signup_view.html' , context)
+            else:
+                user = User.objects.create_user(
+                    username = username,
+                    password = password1,
+                    profile_image = profile_image,
+                    short_desc = short_desc
+                )
+                login(request , user)
+                return redirect('feeds')
+    else:
+        form = SignupForm()
+        context= {
+            'form' : form
+        }
+        return render(request, 'signup_view.html' , context)
