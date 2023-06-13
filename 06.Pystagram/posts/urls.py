971b5d7c77a2f76e5589669acaa4b9afd654e457
GalaDriel <g_ala@naver.com>
2023-06-13 16:28:36 +0900

    Update: LoginView.html

--- 06.Pystagram/posts/urls.py
+++ 06.Pystagram/posts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import feed_view
 
 urlpatterns = [
-    path('feeds/', feed_view),
+    path('feeds/', feed_view , name='feeds'),
 ]
 
--- 06.Pystagram/users/views.py
+++ 06.Pystagram/users/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from .forms import LoginForm
 # Create your views here.
 
 def login_view(request):
-    return render(request, 'login_view.html')
+    if request.method == 'POST':
+        form = LoginForm(data = request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username = username , password = password)
+            if user:
+                login(request , user)
+                return redirect('feeds')
+            else:
+                form.add_error(None, '로그인에 실패 하였습니다.')
+        context={
+            'form':form
+        }
+        return render(request, 'login_view.html' , context)
+    else:
+        form = LoginForm()
+        context={
+            'form':form
+        }
+        return render(request, 'login_view.html' , context)
+    
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
