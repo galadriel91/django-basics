f17b010b1257fda6c95cb1133ae7f4659b1b1190
GalaDriel <g_ala@naver.com>
2023-06-14 23:20:45 +0900

    Update: Define Remove comment

--- 06.Pystagram/posts/urls.py
+++ 06.Pystagram/posts/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import feed_view
+from .views import feed_view , comment_add , remove_comment
 
 urlpatterns = [
     path('feeds/', feed_view, name='feeds'),
+    path('comment_add/', comment_add, name='commentAdd'),
+    path('comment_remove/<int:comment_id>', remove_comment, name='removeComment'),
 ]
--- 06.Pystagram/posts/views.py
+++ 06.Pystagram/posts/views.py
@@ -1,10 +1,31 @@
-from django.shortcuts import render
-from .models import Post
+from django.views.decorators.http import require_POST
+from django.shortcuts import render, redirect
+from .models import Post, Comment
+from .forms import CommentForm
 # Create your views here.
 
 def feed_view(request):
     posts = Post.objects.all()
+    form = CommentForm()
     context={
-        'posts': posts
+        'posts': posts,
+        'form' : form
     }
     return render(request, 'posts/feed_view.html' , context)
+
+@require_POST
+def comment_add(request):
+    form = CommentForm(data = request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.user = request.user
+        comment.save()
+        return redirect('feeds')
+    
+@require_POST
+def remove_comment(request , comment_id):
+    if request.method == 'POST':
+        comment = Comment.objects.get(id = comment_id)
+        if comment.user == request.user:
+            comment.delete()
+            return redirect('feeds')
