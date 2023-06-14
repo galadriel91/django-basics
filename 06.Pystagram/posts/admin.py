78561d5ed7dccc6ad2b7692bb86b1eb3691854f3
GalaDriel <g_ala@naver.com>
2023-06-14 18:34:32 +0900

    Update: Define Posts models.py

--- 06.Pystagram/posts/admin.py
+++ 06.Pystagram/posts/admin.py
@@ -1,3 +1,28 @@
+import admin_thumbnails
 from django.contrib import admin
+from .models import Post, PostImage, Comment
+
+class CommentInline(admin.TabularInline):
+    model=Comment
+    extra =1
+
+@admin_thumbnails.thumbnail('photo')
+class PhotoInline(admin.TabularInline):
+    model=PostImage
+    extra =1
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id' , 'content']
+    inlines = [
+        CommentInline, PhotoInline
+    ]
+
+@admin.register(PostImage)
+class PostImageAdmin(admin.ModelAdmin):
+    list_display = ['id' , 'post' , 'photo']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id' , 'post' , 'content']
--- 06.Pystagram/posts/views.py
+++ 06.Pystagram/posts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
 def feed_view(request):
-    return render(request, 'posts/feed_view.html')
+    posts = Post.objects.all()
+    context={
+        'posts': posts
+    }
+    return render(request, 'posts/feed_view.html' , context)
