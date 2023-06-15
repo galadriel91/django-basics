f5ea8a9497565574426a01f57538d184e317d1ea
GalaDriel <g_ala@naver.com>
2023-06-15 21:57:18 +0900

    Update: Define add_comment

--- 06.Pystagram/posts/urls.py
+++ 06.Pystagram/posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import feeds_view
+from .views import feeds_view, add_comment
 
 urlpatterns = [
     path('feeds/', feeds_view, name='feeds'),
+    path('add_comment/', add_comment, name='addComment'),
 ]
--- 06.Pystagram/posts/views.py
+++ 06.Pystagram/posts/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.views.decorators.http import require_POST
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 def feeds_view(request):
     posts = Post.objects.all()
+    form = CommentForm()
     context={
-        'posts':posts
+        'posts':posts,
+        'form':form
     }
     return render(request , 'posts/feeds_view.html', context)
+
+@require_POST
+def add_comment(request):
+    form = CommentForm(data = request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.user = request.user
+        comment.save()
+        return redirect('feeds')
