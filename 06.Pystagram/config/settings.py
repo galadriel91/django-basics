8bf8a775a5a060f6ec8f9450e047d4a4c6e10871
GalaDriel <g_ala@naver.com>
2023-06-15 19:51:57 +0900

    Update: Define Login & Logout

--- 06.Pystagram/config/settings.py
+++ 06.Pystagram/config/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'posts',
     'users',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -126,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIR = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- 06.Pystagram/users/views.py
+++ 06.Pystagram/users/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate , logout
+from django.shortcuts import render , redirect
+from .forms import LoginForm
 
 # Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(data = request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = authenticate(username = username , password = password)
+            if(user):
+                login(request , user)
+                return redirect('feeds')
+            else:
+                form.add_error(None, '로그인에 실패했습니다.')
+        context={
+            'form':form
+        }
+        return render(request, 'users/login_view.html', context)
+    else:
+        form = LoginForm()
+        context={
+            'form':form
+        }
+        return render(request, 'users/login_view.html', context)
+
+def signup_view(request):
+    return render(request, 'users/signup_view.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
