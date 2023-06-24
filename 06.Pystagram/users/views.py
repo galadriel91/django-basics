52951fbb4a98d38a75858df12e0ff2c78d3e3836
GalaDriel <g_ala@naver.com>
2023-06-24 20:26:58 +0900

    Update: Clean Signup Logic

--- 06.Pystagram/users/views.py
+++ 06.Pystagram/users/views.py
@@ -31,38 +31,15 @@ def signup_view(request):
     if request.method == 'POST':
         forms = SignupForm(data = request.POST, files= request.FILES)
         if forms.is_valid():
-            username = forms.cleaned_data['username']
-            password = forms.cleaned_data['password']
-            password1 = forms.cleaned_data['password1']
-            profile_image = forms.cleaned_data['profile_image']
-            short_desc = forms.cleaned_data['short_desc']
-
-            if password != password1:
-                forms.add_error('password1' , '비밀번호가 맞지 않습니다')
-
-            if User.objects.filter(username = username).exists():
-                forms.add_error(None, '이미 존재하는 회원입니다.')
-
-            if forms.errors:
-                context={
-                    'forms' : forms
-                }
-                return render(request, 'users/signup_view.html' , context)
-            else:
-                user = User.objects.create_user(
-                    username = username,
-                    password = password,
-                    profile_image = profile_image,
-                    short_desc = short_desc
-                )
-                login(request , user)
-                return redirect('feeds')
+            user = forms.save()
+            login(request , user)
+            return redirect('feeds')
     else:
         forms = SignupForm()
-        context={
-            'forms' : forms
-        }
-        return render(request, 'users/signup_view.html' , context)
+    context={
+        'forms' : forms
+    }
+    return render(request, 'users/signup_view.html' , context)
 
 
 def logout_view(request):
