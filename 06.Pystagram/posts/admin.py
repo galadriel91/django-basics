bc262f5f519be4a372d349ef21699a0aaaeae7ab
GalaDriel <g_ala@naver.com>
2023-06-26 23:00:34 +0900

    Update: Define DjangoAdmin - Thumbnails

--- 06.Pystagram/posts/admin.py
+++ 06.Pystagram/posts/admin.py
@@ -1,15 +1,28 @@
+import admin_thumbnails
 from django.contrib import admin
 from .models import Post, PostImage, Comment
 # Register your models here.
 
+class CommentInline(admin.TabularInline):
+    extra = 1
+    model = Comment
+
+@admin_thumbnails.thumbnail('photo')
+class PhotoInline(admin.TabularInline):
+    extra = 1
+    model = PostImage
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id' , 'content']
+    inlines = [
+        CommentInline, PhotoInline
+    ]
 
 @admin.register(PostImage)
 class PostImageAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id' , 'post' , 'photo']
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id' , 'post' , 'content']
